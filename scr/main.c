42c80008bf45a467a739ca8e6457cd8f29f52c93
xe4nor <bachmanndominguezjan@gmail.com>
2025-12-05 16:00:56 +0100

    Update main.c

--- scr/main.c
+++ scr/main.c
@@ -1,1 +1,11 @@
+#include <Windows.h>
 
+#include "window.h"
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE prev, LPSTR cmd, int show) {
+	
+	InitWindow(); // Fenster Registrieren
+	RunMessageLoop(); // Message Loop starten
+
+	return 0;
+}
